7871ca37eb2dedd6d64f2d5dfdea7faaeb773c5e
Cliff Crerar <cliff.crerar@gmail.com>
2023-06-01 01:12:15 +0200

    modified spa proxy

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -1,12 +1,13 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
+console.log(env);
+
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:36098';
 
 const context =  [
-  "/weatherforecast",
-  "/check-user"
+  "/api"
 ];
 
 module.exports = function(app) {
